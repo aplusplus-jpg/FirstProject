20f43a1f9af9c65ed435515ec1b03b6ed441d946
Alejandro González <97806300+xelaalex2898@users.noreply.github.com>
2022-10-22 01:46:00 -0500

    cambios alex

--- Direccion.py
+++ Direccion.py
@@ -1,16 +1,17 @@
-
-from main import *
+from dataclasses import fields
+import pandas as pd
 import requests
-
-    
-def Alrededores_google(dir,nombre):
-    coordenadas=address_to_coordinates(dir)
-    print("dir",dir)
-    print("coord",coordenadas)
-    url=url_google_alrededores(coordenadas[0],coordenadas[1],50,nombre)
-    response=requests.request("GET",url,headers={},data={})
-    return response
+from urllib import response
 
 
-
-
+def busqueda_id_google(id):
+    url="https://maps.googleapis.com/maps/api/place/details/json?place_id="+id
+    fields="&filds=name"
+    token_api_google = "&key=AIzaSyDRhQ9HRGDmnGI6Rd79x1fp-vhCaWoJeYo"
+    payload={}
+    headers = {}
+    url=url+fields+token_api_google
+    response = requests.request("GET", url, headers=headers, data=payload)
+    print(response.text)
+id="Ek5WZXJzbyA4OSwgSmFpbWUgVG9ycmVzIEJvZGV0LCBUbMOhaHVhYywgMTM1MzAgU2FuIEp1YW4gSXh0YXlvcGFuLCBDRE1YLCBNZXhpY28iMBIuChQKEgnlxCWjRhvOhRE5SpX8QUfMgxBZKhQKEgl7nF-9RhvOhRFLFd1H7q8i8g"    
+busqueda_id_google(id)
