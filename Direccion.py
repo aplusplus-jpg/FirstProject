02e1743b3feeb318aabafe57d8e41ce73e7e208d
Alejandro González <97806300+xelaalex2898@users.noreply.github.com>
2022-10-22 12:29:37 -0500

    concatenated dataframe

--- Direccion.py
+++ Direccion.py
@@ -152,6 +152,7 @@ def busqueda_id_google(id):
     fin_tiempo = time.time()
     print("\n\n",fin_tiempo-inicio_tiempo,"""
 
+
 dir="C. Dr. José María Vértiz 1148, Independencia, Benito Juárez, 03630 Ciudad de México, CDMX"
 nom="Farmacia San Pablo Vertiz"
 
--- test_for_batches.py
+++ test_for_batches.py
@@ -1,18 +1,16 @@
 import pandas as pd
 
-#df = pd.read_csv('https://raw.githubusercontent.com/aplusplus-jpg/resources/main/csv1.csv?token=GHSAT0AAAAAABYSA5PPWIJQVIRWAOU7FYQOY2T6P4A', header=0)
-df = pd.read_csv('../resources/csv1.csv', header=0)
-#print(df.info())
 
-df["direccionk"] = df["Direccion1"].map(str)  + " "+ df["Direccion2"].map(str) + " " + df["Direccion3"].map(str) + " " +df["Colonia"].map(str) + " " + df["MunicipioDel"].map(str) + " " + df["Estado"].map(str) + df["CP"].map(str)
-#print(df.head())  
-#print(df.tail())  
+def concatenateDataFrame(path, newPath):
+    """
+    Create a new dataset with concatenated columns
+    """
+    df = pd.read_csv(path, header=0)
+    nf = pd.DataFrame()
+    nf['nombre'] = df['NombComp']
+    nf["direccion"] = df["Direccion1"].map(str)  + " "+ df["Direccion2"].map(str) + " " + df["Direccion3"].map(str) + " " +df["Colonia"].map(str) + " " + df["MunicipioDel"].map(str) + " " + df["Estado"].map(str) + df["CP"].map(str)
+    nf.to_csv(newPath)
 
 
-print(range(len(df.columns) - 1))
-for i in range(len(df.columns) - 1):
-    print(df.columns[i])
-    if df.columns[i] not in ["NombComp", "direccion"]:
-        del df[df.columns[i]]
-
-#print(df.info())
+for i in range(1, 6):
+    concatenateDataFrame(f"../resources/csv{i}.csv", f"../resources/new{i}.csv")
