624dc33d43e455648525d0823e10aa58cb06fa26
Alejandro González <97806300+xelaalex2898@users.noreply.github.com>
2022-10-22 12:57:28 -0500

    Made test for addres to coordinates

--- test_for_batches.py
+++ test_for_batches.py
@@ -1,4 +1,19 @@
+import requests
 import pandas as pd
+
+
+api_key = "AIzaSyD9Jg7-4i7hnAQ7vXaCBaOSBZqdwdloHKg"
+
+
+def address_to_coordinates(address):
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address="+ address +"&key=" + api_key
+    response = requests.request("GET", url, headers={}, data={})
+    try: 
+        coordinates_dict = response.json()['results'][0]['geometry']['location']
+        coordinates_tuple = (coordinates_dict['lat'], coordinates_dict['lng'] )
+        return coordinates_tuple 
+    except IndexError:
+        return (0,0)
 
 
 def concatenateDataFrame(path, newPath):
@@ -9,8 +24,14 @@ def concatenateDataFrame(path, newPath):
     nf = pd.DataFrame()
     nf['nombre'] = df['NombComp']
     nf["direccion"] = df["Direccion1"].map(str)  + " "+ df["Direccion2"].map(str) + " " + df["Direccion3"].map(str) + " " +df["Colonia"].map(str) + " " + df["MunicipioDel"].map(str) + " " + df["Estado"].map(str) + df["CP"].map(str)
+    #nf['geocodes'] = nf["direccion"].map(address_to_coordinates)
     nf.to_csv(newPath)
 
+nd = pd.read_csv("/home/alex/FirstProject/examples.csv", header=0)
+nd["geocodes"] = nd["direccion"].map(address_to_coordinates)
+nd.to_csv('examples.csv')
+print(nd)
 
-for i in range(1, 6):
-    concatenateDataFrame(f"../resources/csv{i}.csv", f"../resources/new{i}.csv")
+
+
+
