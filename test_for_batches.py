48eb78c852742542c0371f1de13dffe88b3bfbde
Alejandro González <97806300+xelaalex2898@users.noreply.github.com>
2022-10-22 19:20:44 -0500

    new dataset

--- test_for_batches.py
+++ test_for_batches.py
@@ -23,14 +23,14 @@ def concatenateDataFrame(path, newPath):
     df = pd.read_csv(path, header=0)
     nf = pd.DataFrame()
     nf['nombre'] = df['NombComp']
-    nf["direccion"] = df["Direccion1"].map(str)  + " "+ df["Direccion2"].map(str) + " " + df["Direccion3"].map(str) + " " +df["Colonia"].map(str) + " " + df["MunicipioDel"].map(str) + " " + df["Estado"].map(str) + df["CP"].map(str)
+    nf["direccion"] = df["Direccion1"].map(str)  + " "+ df["Direccion2"].map(str) + " " + df["Direccion3"].map(str) + " " +df["Colonia"].map(str) + " " + df["MunicipioDel"].map(str) + " " +  df["CP"].map(str) + " " + df["Estado"].map(str)  
     #nf['geocodes'] = nf["direccion"].map(address_to_coordinates)
     nf.to_csv(newPath)
 
-nd = pd.read_csv("/home/alex/FirstProject/examples.csv", header=0)
-nd["geocodes"] = nd["direccion"].map(address_to_coordinates)
-nd.to_csv('examples.csv')
-print(nd)
+
+for i in range(1,7):
+    concatenateDataFrame(f"../resources/csv{i}.csv", f"./new{i}.csv")
+
 
 
 
